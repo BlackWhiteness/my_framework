da1da920359e2c918e6bc417609050a04273bb05
wangshuai <wangshuai@gammainfo.com>
2020-06-11 18:05:44 +0800

    'controller-resolver'

--- public/index.php
+++ public/index.php
@@ -5,15 +5,17 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 
 
-//function render_template(Request $request)
-//{
-//    extract($request->attributes->all(), EXTR_SKIP);
-//    ob_start();
-//    include sprintf(__DIR__ . '/../src/page/%s.php', $_route);
-//    return (new Response())->setContent(ob_get_clean());
-//}
+function render_template(Request $request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__ . '/../src/page/%s.php', $_route);
+    return (new Response())->setContent(ob_get_clean());
+}
 
 $request = Request::createFromGlobals();
 $response = new Response();
@@ -24,13 +26,19 @@ $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 
+$resolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
 try {
+//    $request->attributes->add($matcher->match($request->getPathInfo()));
+//    $controller = $request->attributes->get('_controller');
+//    $controller[0] = new $controller[0]();
+//    ob_start();
+//    call_user_func($controller, $request);
+//    $response->setContent(ob_get_clean());
     $request->attributes->add($matcher->match($request->getPathInfo()));
-    $controller = $request->attributes->get('_controller');
-    $controller[0] = new $controller[0]();
-    ob_start();
-    call_user_func($controller, $request);
-    $response->setContent(ob_get_clean());
+    $controller = $resolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+    call_user_func_array($controller, $arguments);
 } catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
--- src/routes/index.php
+++ src/routes/index.php
@@ -6,11 +6,11 @@ use Symfony\Component\Routing\Route;
 $routes = new RouteCollection();
 
 $routes->add('index',
-    new Route('/index/{name}', ['name' => 'world', '_controller' => [\App\page\Index::class, 'index']]));
-$routes->add('bye', new Route('/bye', ['_controller' => [new \App\page\Bye(), 'index']]));
+    new Route('/index/{name}', ['name' => 'world', '_controller' => '\App\page\Index::index']));
+$routes->add('bye', new Route('/bye', ['_controller' =>'\App\page\Bye::index']));
 $routes->add('year', new Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => [new \App\page\Year(), 'index']
+    '_controller' => '\App\page\Year::index'
 )));
 
 return $routes;
